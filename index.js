ec01e45f8d9d7da9cbff90c2c5ec4ca5fb459eb4
Nico Braun <nico-braun@live.de>
2021-04-13 23:06:59 +0000

    cache response for 1 hour

--- index.js
+++ index.js
@@ -10,18 +10,30 @@ const PROXY_ENDPOINT = "/"
 
 async function handleRequest(request) {
   const url = new URL(request.url)
+  let apiUrl = wHost + url.pathname + url.search
   
-  let apiUrl =  wHost+url.pathname+url.search
-
   request = new Request(apiUrl, request)
   request.headers.set("Origin", new URL(apiUrl).origin)
+
+  const cacheUrl = new URL(request.url)
+  const cacheKey = new Request(cacheUrl.toString(), request)
+  const cache = caches.default
+
+  let response = await cache.match(cacheKey)
+
+  if (response) return response
+
   let response = await fetch(request)
 
   response = new Response(response.body, response)
 
-  response.headers.set("Access-Control-Allow-Origin", url.origin)
+  response.headers.set("Access-Control-Allow-Origin", '*')
 
   response.headers.append("Vary", "Origin")
+
+  response.headers.append("Cache-Control", "s-maxage=3600")
+
+  await cache.put(cacheKey, response.clone())
 
   return response
 }
@@ -37,7 +49,7 @@ function handleOptions(request) {
       ...corsHeaders,
       "Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers"),
     }
-
+    console.log(respHeaders)
     return new Response(null, {
       headers: respHeaders,
     })
@@ -53,27 +65,24 @@ function handleOptions(request) {
 
 addEventListener("fetch", event => {
   const request = event.request
-  const url = new URL(request.url)
-  if (url.pathname.startsWith(PROXY_ENDPOINT)) {
-    if (request.method === "OPTIONS") {
-      // Handle CORS preflight requests
-      event.respondWith(handleOptions(request))
-    }
-    else if (
-      request.method === "GET" ||
-      request.method === "HEAD" ||
-      request.method === "POST"
-    ) {
-      // Handle requests to the API server
-      event.respondWith(handleRequest(request))
-    }
-    else {
-      event.respondWith(
-        new Response(null, {
-          status: 405,
-          statusText: "Method Not Allowed",
-        }),
-      )
-    }
+  if (request.method === "OPTIONS") {
+    // Handle CORS preflight requests
+    event.respondWith(handleOptions(request))
+  }
+  else if (
+    request.method === "GET" ||
+    request.method === "HEAD" ||
+    request.method === "POST"
+  ) {
+    // Handle requests to the API server
+    event.respondWith(handleRequest(request))
+  }
+  else {
+    event.respondWith(
+      new Response(null, {
+        status: 405,
+        statusText: "Method Not Allowed",
+      }),
+    )
   }
 })
